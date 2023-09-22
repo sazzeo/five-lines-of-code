d296755958962eeec5459bea8d4bbd37a8cc914f
jiyeong-dt <imjee02@gmail.com>
2023-09-22 23:59:12 +0900

    feat: drawMap, drawPlayer 메소드 추출

--- index.ts
+++ index.ts
@@ -118,7 +118,12 @@ function draw() {
 
   g.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw map
+  drawMap(g);
+  drawPlayer(g);
+
+}
+
+const drawMap = (g : CanvasRenderingContext2D)=>{
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
       if (map[y][x] === Tile.FLUX)
@@ -138,8 +143,9 @@ function draw() {
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
   }
+}
 
-  // Draw player
+const drawPlayer =(g : CanvasRenderingContext2D)=>{
   g.fillStyle = "#ff0000";
   g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
