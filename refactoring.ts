babe6b0fabfdfe1f3b4797fbef771c1d39f9c7b3
jiyeong-dt <imjee02@gmail.com>
2023-09-23 00:49:22 +0900

    feat: 메소드 추출 예제

--- refactoring.ts
+++ refactoring.ts
@@ -1,39 +1,44 @@
 const minimum = (arr: number[][]) => {
-    let result = Number.POSITIVE_INFINITY;
-    for (let x = 0; x < arr.length; x++) {
-        for (let y = 0; y < arr[x].length; y++) {
-            if (result > arr[x][y]) {
-                result = arr[x][y];
-            }
-        }
+  let result = Number.POSITIVE_INFINITY;
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr[x].length; y++) {
+      result = min(result, arr, x, y);
     }
-    return result;
+  }
+  return result;
+}
+
+const min = (result: number, arr: number[][], x: number, y: number) => {
+  if (result > arr[x][y]) {
+    return arr[x][y];
+  }
 }
 
 
 const sum = (arr: number[]): number => {
-    return arr.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+  return arr.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
 }
 
 const average = (arr: number[]): number => {
-    return sum(arr) / arr.length;
+  return sum(arr) / arr.length;
 }
 
+
 const isPrime = (n: number): boolean => {
-    for(let i =2; i <= Math.sqrt(n) ; i++) {
-        if(n%i ===0 ) {
-            return false;
-        }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 const reportPrimes = (n: number) => {
-    for (let i = 2; i < n; i++) {
-        if (isPrime(i)) {
-            console.log(`${i} is prime`);
-        }
+  for (let i = 2; i < n; i++) {
+    if (isPrime(i)) {
+      console.log(`${i} is prime`);
     }
+  }
 }
 
 reportPrimes(30);
